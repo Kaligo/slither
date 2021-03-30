9f6d3e83c3dee597f3ea8c0e65318e4cb553ef33
marfoldi <peter.marfoldi@kaligo.com>
2021-03-30 14:52:16 +0200

    Fix `generator.rb` indentation

--- lib/slither/generator.rb
+++ lib/slither/generator.rb
@@ -1,26 +1,27 @@
 class Slither
   class Generator
+    def initialize(definition)
+      @definition = definition
+    end
 
-		def initialize(definition)
-			@definition = definition
-		end
+    def generate(data)
+      @builder = []
+      @definition.sections.each do |section|
+        content = data[section.name]
+        if content
+          content = [content] if content.is_a?(Hash)
+          raise(Slither::RequiredSectionEmptyError, "Required section '#{section.name}' was empty.") unless content.any?
 
-		def generate(data)
-	    @builder = []
-	    @definition.sections.each do |section|
-	      content = data[section.name]
-	      if content
-  	      content = [content] if content.is_a?(Hash)
-  	      raise(Slither::RequiredSectionEmptyError, "Required section '#{section.name}' was empty.") unless content.any?
-  	      content.each do |row|
-  	        @builder << section.format(row)
-  	      end
-  	    else
-  	      raise(Slither::RequiredSectionEmptyError, "Required section '#{section.name}' was empty.") unless section.optional
-	      end
-	    end
-	    @builder.join("\n")
-		end
-
-	end
+          content.each do |row|
+            @builder << section.format(row)
+          end
+        else
+          unless section.optional
+            raise(Slither::RequiredSectionEmptyError, "Required section '#{section.name}' was empty.")
+          end
+        end
+      end
+      @builder.join("\n")
+    end
+  end
 end
