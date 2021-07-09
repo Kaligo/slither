775d452592a05724cd969bf5a21586931985d40f
marfoldi <peter.marfoldi@kaligo.com>
2021-07-09 15:37:38 +0200

    [LOYAL-1784] Update `Slither::Generator#generate` w/ optional section check

--- lib/slither/generator.rb
+++ lib/slither/generator.rb
@@ -8,22 +8,14 @@ class Slither
       @builder = []
       @definition.sections.each do |section|
         content = data[section.name]
-        content = [content] if content.is_a?(Hash) || content.nil?
-        raise(Slither::RequiredSectionEmptyError, "Required section '#{section.name}' was empty.") if !content || empty_required_content(content, section)
+        raise(Slither::RequiredSectionEmptyError, "Required section '#{section.name}' was empty.") if !content&.any? && !section.optional
 
-        if content.any?
-          content.each do |row|
-            @builder << section.format(row)
-          end
+        content = [content] if content.is_a?(Hash)
+        content&.each do |row|
+          @builder << section.format(row)
         end
       end
       @builder.join("\n")
-    end
-
-    private
-
-    def empty_required_content(content, section)
-      content.none? && !section.optional
     end
   end
 end
